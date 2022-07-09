7dbf0e1f97ebc71871319c26935ca82e957e503a
ahmed albar <81378817+fatalbar@users.noreply.github.com>
2022-07-10 03:21:02 +0700

    Create subquery.sh

---
+++ Subquery/subquery.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo "=================================================="
+echo -e "\033[0;35m"
+echo " ' ███╗░░░███╗███████╗░█████╗░░██╗░░░░░░░██╗██████╗░░█████╗░██████╗░███████╗██████╗░ ";
+echo " ' ████╗░████║██╔════╝██╔══██╗░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗ ";
+echo " ' ██╔████╔██║█████╗░░██║░░██║░╚██╗████╗██╔╝██████╦╝██║░░██║██████╔╝█████╗░░██║░░██║ ";
+echo " ' ██║╚██╔╝██║██╔══╝░░██║░░██║░░████╔═████║░██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░██║ ";
+echo " ' ██║░╚═╝░██║███████╗╚█████╔╝░░╚██╔╝░╚██╔╝░██████╦╝╚█████╔╝██║░░██║███████╗██████╔╝ ";
+echo " ' ╚═╝░░░░░╚═╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░ ";
+echo -e "\e[0m"
+echo "=================================================="
+sleep 2
+
+# 1. Update packages
+sudo apt update && sudo apt upgrade -y
+
+# 2. Install dependencies
+sudo apt install wget jq build-essential nano unzip -y
+
+# 3. Install docker
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# 4. Install docker compose
+docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
+sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
+sudo chmod +x /usr/bin/docker-compose
+
+# 5. Set up firewall
+sudo apt install ufw -y
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 8000
+sudo ufw allow 9090
+sudo ufw allow 9100
+sudo ufw allow 3000:3100/tcp
+
+# 6. Install SubQuery
+cd $HOME
+mkdir testnet && cd testnet
+wget -qO docker-compose.yml https://raw.githubusercontent.com/subquery/indexer-services/main/docker-compose.yml
+docker-compose up -d
+
+# Node setup finished!
+
+dashboard_url="http://$(wget -qO- eth0.me):8000"
+
+echo "==================================================="
+echo -e '\e[32mNode setup finished!\e[39m' && sleep 1
+echo -e "You can access your Dashboard: \e[32m$dashboard_url\e[39m"
+sleep 2
