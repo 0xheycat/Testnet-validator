7ac20e5327c7ae416252a8b985dd5298df831022
Fata albar <81378817+fatalbar@users.noreply.github.com>
2022-09-27 07:51:41 +0700

    Create mun.sh

---
+++ MUN/mun.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+echo "================================================================================"
+echo -e "\033[0;35m"
+echo " ' ███╗░░░███╗███████╗░█████╗░░██╗░░░░░░░██╗██████╗░░█████╗░██████╗░███████╗██████╗░ ";
+echo " ' ████╗░████║██╔════╝██╔══██╗░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗ ";
+echo " ' ██╔████╔██║█████╗░░██║░░██║░╚██╗████╗██╔╝██████╦╝██║░░██║██████╔╝█████╗░░██║░░██║ ";
+echo " ' ██║╚██╔╝██║██╔══╝░░██║░░██║░░████╔═████║░██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░██║ ";
+echo " ' ██║░╚═╝░██║███████╗╚█████╔╝░░╚██╔╝░╚██╔╝░██████╦╝╚█████╔╝██║░░██║███████╗██████╔╝ ";
+echo " ' ╚═╝░░░░░╚═╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░ ";
+echo -e "\e[0m"
+echo "================================================================================="
+
+sleep 2
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Validator Name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export MUN_CHAIN_ID=testmun" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Your node name: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Your chain name: \e[1m\e[32m$MUN_CHAIN_ID\e[0m"
+echo '================================================='
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install build-essential jq -y
+
+# install go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/munblockchain/mun
+cd mun
+sudo rm -rf ~/.mun
+go mod tidy
+make install
+
+clear
+
+mkdir -p ~/.mun/upgrade_manager/upgrades
+mkdir -p ~/.mun/upgrade_manager/genesis/bin
+
+# genesys update
+cp $(which mund) ~/.mun/upgrade_manager/genesis/bin
+sudo cp $(which mund-manager) /usr/bin
+
+# init
+mund init $NODENAME --chain-id testmun
+
+#Fetch genesis
+curl --tlsv1 https://node1.mun.money/genesis? | jq ".result.genesis" > ~/.mun/config/genesis.json
+
+# update seed
+seeds = "b4eeaf7ca17e5186b181885714cedc6a78d20c9b@167.99.6.48:26656"
+sed -i 's/stake/utmun/g' ~/.mun/config/genesis.json
+
+# create service
+sudo tee /etc/systemd/system/mund.service > /dev/null <<EOF
+[Unit]
+Description=mund
+Requires=network-online.target
+After=network-online.target
+[Service]
+User=$USER
+Restart=on-failure
+RestartSec=3
+Environment=DAEMON_NAME=mund
+Environment=DAEMON_HOME=/$HOME/.mun
+Environment=DAEMON_ALLOW_DOWNLOAD_BINARIES=on
+Environment=DAEMON_RESTART_AFTER_UPGRADE=on
+PermissionsStartOnly=true
+ExecStart=/usr/bin/mund-manager start --pruning="nothing" --rpc.laddr "tcp://0.0.0.0:26757"
+StandardOutput=file:/var/log/mund/mund.log
+StandardError=file:/var/log/mund/mund_error.log
+LimitNOFILE=4096
+[Install]
+WantedBy=multi-user.target
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable mund
+sudo systemctl restart mund
+
+echo '=============== SETUP FINISHED ==================='
+echo -e 'To check logs: \e[1m\e[32msudo journalctl -u mund -f -o cat\e[0m'
+echo -e "To check sync status: \e[1m\e[32mmund status 2>&1 | jq .SyncInfo\e[0m" && sleep 1
