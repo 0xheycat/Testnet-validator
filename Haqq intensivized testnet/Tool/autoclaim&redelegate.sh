049d60e58f30703da089fcd2d5670e069958a7a8
Fata albar <81378817+fatalbar@users.noreply.github.com>
2022-10-02 00:56:23 +0700

    Update autoclaim&redelegate.sh

--- Haqq intensivized testnet/Tool/autoclaim&redelegate.sh
+++ Haqq intensivized testnet/Tool/autoclaim&redelegate.sh
@@ -9,28 +9,28 @@ echo " ' ██║░╚═╝░██║███████╗╚███�
 echo " ' ╚═╝░░░░░╚═╝╚══════╝░╚════╝░░░░╚═╝░░░╚═╝░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░ ";
 echo -e "\e[0m"
 echo "================================================================================="
-echo 'Welcome to autoscript claim staking reward from your NODE'
 sleep 2
 
 # set your Automatic claim staking reward
 if [ ! $NODE ]; then
 read -p "Input your Validator name: " NODE
+echo 'export NODE='\"${NODE}\" >> $HOME/.bash_profile
 if [ ! $ADDRESS ]; then
 read -p "Input your wallet address: " ADDRESS
+echo 'export ADDRESS='\"${ADDRESS}\" >> $HOME/.bash_profile
 if [ ! $VALOPER ]; then
 read -p "Input your Validator address : " VALOPER
+echo 'export VALOPER='\"${VALOPER}\" >> $HOME/.bash_profile
 if [ ! $CHAIN ]; then
 read -p "Input your Chain ID,ex chain for HAQQ is haqq_54211-2 : " CHAIN
+echo 'export CHAIN='\"${CHAIN}\" >> $HOME/.bash_profile
 if [ ! $TOKEN ]; then
 read -p "Input your Token name,ex= usei for SEI,aISLM for HAQQ : " TOKEN
+echo 'export TOKEN='\"${TOKEN}\" >> $HOME/.bash_profile
 if [ ! $SERVICE ]; then
 read -p "Input Service NAME your NODE Testnet,ex haqqd,seid,mund,masad : " SERVICE
-echo 'export NODE='\"${NODE}\" >> $HOME/.bash_profile
 echo 'export SERVICE='\"${SERVICE}\" >> $HOME/.bash_profile
-echo 'export TOKEN='\"${TOKEN}\" >> $HOME/.bash_profile
-echo 'export ADDRESS='\"${ADDRESS}\" >> $HOME/.bash_profile
-echo 'export VALOPER='\"${VALOPER}\" >> $HOME/.bash_profile
-echo 'export CHAIN='\"${CHAIN}\" >> $HOME/.bash_profile
+
 fi
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 . $HOME/.bash_profile
@@ -49,6 +49,7 @@ sleep 1
 if [ ! -e $SERVICE status 2>&1 | jq .SyncInfo ]; then
   echo "Your NODE is Running Well";
 fi
+chmod +x $HOME/autoclaim%26redelegate.sh
 cd $HOME
 claim=$SERVICE tx distribution withdraw-all-rewards --from=$ADDRESS --chain-id=$CHAIN -y
 if [ $claim ]; then
@@ -65,7 +66,7 @@ cd $HOME
 delegate=$SERVICE tx staking delegate $VALOPER 500000000000 $TOKEN --from=$ADDRESS --chain-id=$CHAIN -y
 if [ $delegate ]; then
         resp3=$($SERVICE tx staking delegate $VALOPER 500000000000 $TOKEN --from=$ADDRESS --chain-id=$CHAIN -y )
-        echo $resp2
+        echo $resp3
 fi
 
 printf "sleep"
