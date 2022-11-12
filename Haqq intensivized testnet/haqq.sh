64d080194cac05eaa33d5b3c2568a23c264e0302
Fata albar <81378817+fatalbar@users.noreply.github.com>
2022-11-12 12:06:34 +0700

    Update haqq.sh

--- Haqq intensivized testnet/haqq.sh
+++ Haqq intensivized testnet/haqq.sh
@@ -99,6 +99,28 @@ EOF
 
 haqqd tendermint unsafe-reset-all --home $HOME/.haqqd --keep-addr-book
 
+# create sync and  snapshot
+SNAP_RPC="https://haqq-testnet.nodejumper.io:443"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height)
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000))
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.haqqd/config/config.toml
+
+cp $HOME/.haqqd/data/priv_validator_state.json $HOME/.haqqd/priv_validator_state.json.backup
+haqqd tendermint unsafe-reset-all --home $HOME/.haqqd --keep-addr-book
+
+rm -rf $HOME/.haqqd/data 
+
+SNAP_NAME=$(curl -s https://snapshots1-testnet.nodejumper.io/haqq-testnet/ | egrep -o ">haqq_54211-3.*\.tar.lz4" | tr -d ">")
+curl https://snapshots1-testnet.nodejumper.io/haqq-testnet/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.haqqd
+
+mv $HOME/.haqqd/priv_validator_state.json.backup $HOME/.haqqd/data/priv_validator_state.json
 
 # start service
 sudo systemctl daemon-reload
